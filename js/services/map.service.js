712390dae2b3c0845399b7572673a63561778e0a
Oded Kovo <odedkovo2@gmail.com>
2021-12-22 14:12:15 +0200

    hj

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -15,6 +15,23 @@ function initMap(lat = 32.0749831, lng = 34.9120554) {
       zoom: 15,
     });
     console.log('Map!', gMap);
+
+    const myLatlng = { lat, lng };
+    let infoWindow = new google.maps.InfoWindow({
+      content: 'Click the map to get Lat/Lng!',
+      position: myLatlng,
+    });
+    infoWindow.open(gMap);
+    gMap.addListener('click', (ev) => {
+      // Close the current InfoWindow.
+      infoWindow.close();
+      // Create a new InfoWindow.
+      infoWindow = new google.maps.InfoWindow({
+        position: ev.latLng,
+      });
+      infoWindow.setContent(JSON.stringify(ev.latLng.toJSON(), null, 2));
+      infoWindow.open(gMap);
+    });
   });
 }
 
