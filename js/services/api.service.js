0949689ce1bf6f2bcaed784d437163a0a3b01fed
Oded Kovo <odedkovo2@gmail.com>
2021-12-22 17:17:10 +0200

    with geocode api key

--- js/services/api.service.js
+++ js/services/api.service.js
@@ -1,3 +1,3 @@
 const API_KEY = 'AIzaSyCyEVnrvvJpRys_Tzl0T2eg79GxEVj0JlQ'; //TODO: Enter your API Key
-
-export const apiService = { API_KEY };
+const GEOCODEAPI_KEY = 'AIzaSyCDmdV8gdfpWadDpzdDjLgmfn2XTZcyVV8';
+export const apiService = { API_KEY, GEOCODEAPI_KEY };
